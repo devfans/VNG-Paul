268bc58b334d7d9619669e3803e3427f9e14c76b
Stefan Liu <stefanliu@outlook.com>
2017-03-27 21:36:26 +0800

    Fix aligning and set some limits

--- Analyze.py
+++ Analyze.py
@@ -9,42 +9,120 @@ from Compute import DATA
 
 logger = logging
 
+class InvalidCompareError(Exception): pass
+class IndecentCompareError(Exception): pass
+
+
 class Painter(object):
     @classmethod
     def setup(cls, config):
         cls.font = config.get('general', 'font')
-        cls.nameSize = 80
-        cls.nameMaxWidth = 300
-        cls.winnerPoint = (211, 54)
-        cls.loserPoint = (776, 54)
-
-        cls.color = (0, 0, 0, 255)
+        cls.nameFont = config.get('general', 'name_font')
+        cls.nameSize = 55
+        cls.totalSize = 40
         cls.size = 30
+        cls.winnerPoints = [(248, 112), (248, 159), (248, 207)]
+        cls.loserPoints = [(742, 112), (742, 159), (742, 207)]
+
+        cls.color = (255,255,255, 255)
+        cls.winnerColor = (255,180,0, 255)
+        cls.loserColor = (186,0,219, 255)
+
+        cls.scoreLength = 2
+
 
     def __init__(self, c):
         winner, loser = (c.left, c.right) if c.left.WIN else (c.right, c.left)
         self.points = []
-        winnerName = winner.getName()
-        loserName = loser.getName()
-        winnerPoint, winnerSize = self.calculateName(winnerName, winner=True)
-        loserPoint, loserSize = self.calculateName(loserName, winner=False)
-        winnerColor = (253,130,0, 255)
-        loserColor = (255,255,255, 255)
-        self.points.append({"position": winnerPoint, "content": winnerName, "size": winnerSize, 'color': winnerColor})
-        self.points.append({"position": (115, 113), "content": winner.getKills(), 'color': winnerColor})
-        self.points.append({"position": (115, 162), "content": winner.getDeaths(), 'color': winnerColor})
-        self.points.append({"position": (115, 212), "content": winner.getAssists(), 'color': winnerColor})
-        self.points.append({"position": (115, 254), "content": winner.getFarm(), 'color': winnerColor})
-        self.points.append({"position": (115, 306), "content": winner.getWins(), 'color': winnerColor})
-        self.points.append({"position": (106, 345), "content": winner.getTotal(), "size": 45, 'color': winnerColor})
 
-        self.points.append({"position": loserPoint, "content": loserName, "size": loserSize, 'color': loserColor})
-        self.points.append({"position": (662, 113), "content": loser.getKills(), 'color': loserColor})
-        self.points.append({"position": (662, 162), "content": loser.getDeaths(), 'color': loserColor})
-        self.points.append({"position": (662, 212), "content": loser.getAssists(), 'color': loserColor})
-        self.points.append({"position": (662, 254), "content": loser.getFarm(), 'color': loserColor})
-        self.points.append({"position": (662, 306), "content": loser.getWins(), 'color': loserColor})
-        self.points.append({"position": (650, 345), "content": loser.getTotal(), "size": 45, 'color': loserColor})
+        if len(winner.valid) < 2:
+            name = "Unknow" if len(winner.valid) < 1 else winner.valid[0]
+            pos = self.calculatePosition(name, self.winnerPoints[1], self.nameSize, self.nameFont)
+            self.points.append({"position": pos, "content": name, "size": self.nameSize, "color": self.winnerColor, "font": self.nameFont})
+        elif len(winner.valid) < 3:
+            for i in xrange(2):
+                pos = self.calculatePosition(winner.valid[i], self.winnerPoints[i], self.nameSize, self.nameFont)
+                self.points.append({"position": pos, "content": winner.valid[i], "size": self.nameSize, "color": self.winnerColor, "font": self.nameFont})
+        else:
+            for i in xrange(len(self.winnerPoints)):
+                pos = self.calculatePosition(winner.valid[i], self.winnerPoints[i], self.nameSize, self.nameFont)
+                self.points.append({"position": pos, "content": winner.valid[i], "size": self.nameSize, "color": self.winnerColor, "font": self.nameFont})
+
+
+        tag, value = winner.getWins()
+        self.points.append({"position": (77, 316), "content": tag})
+        pos = self.calculatePosition(value, (431, 316), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value})
+        tag, value = winner.getKills()
+        self.points.append({"position": (77, 357), "content": tag})
+        pos = self.calculatePosition(value, (431, 357), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value})
+        tag, value = winner.getDeaths()
+        self.points.append({"position": (77, 400), "content": tag})
+        pos = self.calculatePosition(value, (431, 400), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value})
+        tag, value = winner.getAssists()
+        self.points.append({"position": (77, 441), "content": tag})
+        pos = self.calculatePosition(value, (431, 441), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value})
+        tag, value = winner.getFarm()
+        self.points.append({"position": (77, 485), "content": tag})
+        pos = self.calculatePosition(value, (431, 485), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value})
+        tag, value = winner.getTotal()
+        self.points.append({"position": (77, 591), "content": tag, "size": self.totalSize})
+        pos = self.calculatePosition(value, (431, 591), self.totalSize, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value, "size": self.totalSize})
+
+        tag, value = winner.getNN()
+        self.points.append({"position": (77, 644), "content": tag, "size": self.totalSize})
+        pos = self.calculatePosition(value, (431, 644), self.totalSize, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value, "size": self.totalSize})
+
+        if len(loser.valid) < 2:
+            name = "Unknow" if len(loser.valid) < 1 else loser.valid[0]
+            pos = self.calculatePosition(name, self.loserPoints[1], self.nameSize, self.nameFont)
+            self.points.append({"position": pos, "content": name, "size": self.nameSize, "color": self.loserColor, "font": self.nameFont})
+        elif len(loser.valid) < 3:
+            for i in xrange(2):
+                pos = self.calculatePosition(loser.valid[i], self.loserPoints[i], self.nameSize, self.nameFont)
+                self.points.append({"position": pos, "content": loser.valid[i], "size": self.nameSize, "color": self.loserColor, "font": self.nameFont})
+        else:
+            for i in xrange(len(self.loserPoints)):
+                pos = self.calculatePosition(loser.valid[i], self.loserPoints[i], self.nameSize, self.nameFont)
+                self.points.append({"position": pos, "content": loser.valid[i], "size": self.nameSize, "color": self.loserColor, "font": self.nameFont})
+
+        tag, value = loser.getWins()
+        self.points.append({"position": (571, 316), "content": tag})
+        pos = self.calculatePosition(value, (925, 316), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value})
+        tag, value = loser.getKills()
+        self.points.append({"position": (571, 357), "content": tag})
+        pos = self.calculatePosition(value, (925, 357), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value})
+        tag, value = loser.getDeaths()
+        self.points.append({"position": (571, 400), "content": tag})
+        pos = self.calculatePosition(value, (925, 400), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value})
+        tag, value = loser.getAssists()
+        self.points.append({"position": (571, 441), "content": tag})
+        pos = self.calculatePosition(value, (925, 441), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value})
+        tag, value = loser.getFarm()
+        self.points.append({"position": (571, 485), "content": tag})
+        pos = self.calculatePosition(value, (925, 485), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value})
+        tag, value = loser.getTotal()
+        self.points.append({"position": (571, 591), "content": tag, "size": self.totalSize})
+        pos = self.calculatePosition(value, (925, 591), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value, "size": self.totalSize})
+
+        tag, value = loser.getNN()
+        self.points.append({"position": (571, 644), "content": tag, "size": self.totalSize})
+        pos = self.calculatePosition(value, (925, 644), self.size, self.font, fromEnd=True)
+        self.points.append({"position": pos, "content": value, "size": self.totalSize})
+
+
 
 
     def draw(self, draw):
@@ -52,30 +130,20 @@ class Painter(object):
             font = point['font'] if 'font' in point else self.font
             size = point['size'] if 'size' in point else self.size
             color = point['color'] if 'color' in point else self.color
-
             fmt = ImageFont.truetype(font, size)
             draw.text(point['position'], point['content'], font=fmt, fill=color)
 
-    def calculateName(self, name, winner=True):
+    def calculatePosition(self, text, point, size, font, fromEnd=False):
         canvas = Image.new('RGBA', (1000,1000), (255,255,255, 0))
         draw = ImageDraw.Draw(canvas)
+        font = ImageFont.truetype(font, size)
+        w, h = draw.textsize(text, font)
+        print("%s %s")
 
-        point = self.winnerPoint if winner else self.loserPoint
-        size = self.nameSize
-        w, h = 0, 0
+        if fromEnd:
+            return (point[0] - w, point[1])
+        return (point[0] - w/2, point[1] - h/2)
 
-        while True:
-            font = ImageFont.truetype(self.font, size)
-            w, h = draw.textsize(name, font)
-            if w > self.nameMaxWidth:
-                size -= 2
-            else:
-                break
-
-        if winner:
-            return (self.winnerPoint[0] - w/2, self.winnerPoint[1] - h/2), size
-        else:
-            return (self.loserPoint[0] - w/2, self.loserPoint[1] - h/2), size
 
 
 class Paul(object):
@@ -85,13 +153,14 @@ class Paul(object):
         cls.background = os.path.join(cls.image_dir, config.get('general', 'background'))
 
         Painter.setup(config)
+        cls.invalidCompare = "Please enter a 1v1 or 3v3 matchup! Thanks!"
 
     @classmethod
     def feed(cls, inputText):
         RAW = str(inputText)
         f = RAW.lower().find(Compare.key)
         if f == -1:
-            raise Exception("Invalid vs question!")
+            raise InvalidCompareError()
 
         l = Team(RAW[:f])
         r = Team(RAW[f+len(Compare.key):])
@@ -103,6 +172,9 @@ class Paul(object):
     def analyze(cls, c):
         Team.compute(c.left)
         Team.compute(c.right)
+        VS = (len(c.left.members), len(c.right.members))
+        if VS != (1, 1) and VS != (3, 3):
+            raise IndecentCompareError()
         if c.left.score > c.right.score:
             c.left.WIN = True
             c.winComment = c.left.getName() + ' will win.'
@@ -117,21 +189,7 @@ class Paul(object):
         c.invalid = c.left.invalid + c.right.invalid
         if len(c.invalid) > 0:
             c.unknownComment = '<Paul can not recognize %s' % ', '.join(c.invalid) + '>'
-        c.detailComment = """      Summary
-name: %s vs %s
-Kill Rate: %s vs %s
-Death Rate: %s vs %s
-Win Rate: %s vs %s
-Assist Rate: %s vs %s
-Farming: %s vs %s
-Total Score: %s vs %s""" % \
-(c.left.getName(), c.right.getName(),
- c.left.kills, c.right.kills,
- c.left.deaths, c.right.deaths,
- c.left.wins, c.right.wins,
- c.left.assists, c.right.assists,
- c.left.farm, c.right.farm,
- c.left.score, c.right.score)
+        c.detailComment = "No details for now"
         return c
 
     @classmethod
@@ -158,7 +216,8 @@ Total Score: %s vs %s""" % \
                 out.save(png, 'PNG')
                 png.seek(0)
                 return c.comment, png, True
-
+        except (InvalidCompareError, IndecentCompareError) as e:
+            return cls.invalidCompare, None, False
         except Exception as e:
             logger.exception(e)
 
@@ -188,22 +247,27 @@ class Team(object):
         return '+'.join(self.valid) if len(self.valid) > 0 else 'Unknown'
 
     def getKills(self):
-        return 'Kills Score: ' + str(self.kills)[:6]
+        return 'KILL SCORE', self.trimScore(self.kills)
 
     def getDeaths(self):
-        return 'Deaths Score: ' + str(self.deaths)[:6]
+        return 'DEATH SCORE', self.trimScore(self.deaths)
 
     def getAssists(self):
-        return 'Assists Score: ' + str(self.assists)[:6]
+        return 'ASSIST SCORE', self.trimScore(self.assists)
 
     def getWins(self):
-        return 'Defeating Score: ' + str(self.wins)[:6]
+        return 'WIN SCORE', self.trimScore(self.wins)
 
     def getTotal(self):
-        return 'Total Score: ' + str(self.score)[:6]
+        return 'TOTAL SCORE', self.trimScore(self.score)
 
     def getFarm(self):
-        return 'Farming Score: ' + str(self.farm)[:6]
+        return 'FARMING SCORE', self.trimScore(self.farm)
+
+    def trimScore(self, score):
+        return "%.2f" % float(score)
+
+
 
 
 
