a3a3bf42ac270cfa372ad40ad73f159f85e1ba46
gpz22 <gpz22@hotmail.co.jp>
2017-03-27 17:38:13 +0800

    Added Neural Network Trainer

---
+++ nn/trainer.py
@@ -0,0 +1,132 @@
+import sys
+import os
+import json
+import pickle
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+
+from utils import heroNameToInt, level, xp, ranked, lossStreak, winStreak, wins, played
+
+
+print "Loading matches data..."
+
+files = os.listdir('Matches/') # Previously collected and prepared matches data
+matches = []
+
+for file in files:
+    with open('Matches/' + file, 'r') as f:
+        matches.append(json.loads(f.read()))
+
+print "Matches loaded"
+
+print "Trimming the data..."
+X = []
+Y = []
+
+total = 0
+for match in matches:
+    # Just keep standard and battle royale matches that were completed 
+    if match['data']['attributes']['stats']['endGameReason'] != "victory" or match['data']['attributes']['stats']['queue'] not in ['ranked', 'casual', 'casual_aral']:
+        continue
+		
+    matchData = []
+    resultData = None
+    for roster_id, roster in match['rosters'].iteritems():
+        if resultData is None:
+            resultData = [1.0 if roster['winner'] else 0.0]
+            Y.append(resultData)
+            total = total + 1
+        for participant_id, participant in roster['participants'].iteritems():
+            matchData += [ wins(participant['player']['attributes']['stats']['wins']),
+                       ranked(participant['player']['attributes']['stats']['played_ranked']),
+                       winStreak(participant['player']['attributes']['stats']['winStreak']),
+                       played(participant['player']['attributes']['stats']['played']),
+                       lossStreak(participant['player']['attributes']['stats']['lossStreak']),
+                       level(participant['player']['attributes']['stats']['level']),
+                       xp(participant['player']['attributes']['stats']['xp']),
+                       float(heroNameToInt(participant['attributes']['actor']))
+                     ]
+
+    X.append(matchData)
+
+print "Total matches kept: {}".format(total)
+
+print "Neural network training..."
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(len(X[0]),96),
+            nn.Sigmoid(),
+            nn.Dropout(p=0.1),
+            nn.Linear(96,64),
+            nn.ReLU(),
+            nn.Dropout(p=0.1),
+            nn.Linear(64,32),
+            nn.Sigmoid(),
+            nn.Dropout(p=0.1),
+            nn.Linear(32,16),
+            nn.ReLU(),
+            #nn.Dropout(p=0.1),
+            nn.Linear(16,8),
+            nn.Sigmoid(),
+            #nn.Dropout(p=0.1),
+            nn.Linear(8,1)
+        )
+
+    def forward(self, input):
+        return self.main.forward(input)
+		
+if os.path.exists("nn.dat"):
+    print "Previous neural network found, trying to pick up work where left..."
+    with open("nn.dat", "r") as nn_dat:
+        net = pickle.load(nn_dat)
+else:
+    net = Net()
+net.cuda()
+net.train()
+
+optimizer = optim.Adam(net.parameters(), lr=0.0001)
+#optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+criterion = nn.MSELoss()
+
+input = Variable(torch.FloatTensor(X).cuda())
+target = Variable(torch.FloatTensor(Y).cuda())
+
+print "Starting training..."
+
+for i in xrange(100000000): # arbitrary limit
+
+    optimizer.zero_grad()
+    output = net.forward(input)
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer.step()
+
+    l = loss.data[0] # current loss
+    if l < 0.1:
+        break
+
+    if(i % 100 == 99):
+        print "Iteration={} Loss={}".format(i, loss.data[0])
+
+print "Training finished"
+
+net.eval()
+
+# Verification
+#output = net.forward(Variable(torch.FloatTensor([X[2]]).cuda()))
+#print(Y[2])
+#print(output.data[0])
+
+print "Saving neural network on disk..."
+
+with open("nn.dat", "w") as nn_dat:
+    pickle.dump(net, nn_dat)
+	
+print "Neural network saved"
---
+++ nn/utils.py
@@ -0,0 +1,102 @@
+
+
+# Map a value from one range to another
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    py_clip = lambda x, l, u: l if x < l else u if x > u else x
+
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return py_clip(rightMin + (valueScaled * rightSpan), rightMin, rightMax)
+	
+
+def level(x):
+   return float(translate(int(x), 1, 30, 0, 1))
+
+def xp(x):
+    return float(translate(int(x), 0, 1000000, 0, 1))
+
+def ranked(x):
+    return float(translate(int(x), 0, 10000, 0, 1))
+
+def lossStreak(x):
+    return float(translate(int(x), 0, 1000, 0, 1))
+
+def winStreak(x):
+   return float(translate(int(x), 0, 1000, 0, 1))
+
+def wins(x):
+    return float(translate(int(x), 0, 100000, 0, 1))
+
+def played(x):
+    return float(translate(int(x), 0, 1000000, 0, 1))	
+
+	
+def heroNameToInt(hero):
+    if "Grumpjaw" in hero:
+        return 0
+    elif "Idris" in hero:
+        return 1
+    elif "Flicker" in hero:
+        return 2
+    elif "Gwen" in hero:
+        return 3
+    elif "Baron" in hero:
+        return 4
+    elif "Samuel" in hero:
+        return 5
+    elif "Lyra" in hero:
+        return 6
+    elif "Lance" in hero:
+        return 7
+    elif "Alpha" in hero:
+        return 8
+    elif "Ozo" in hero:
+        return 9
+    elif "Reim" in hero:
+        return 10
+    elif "Kestrel" in hero:
+        return 11
+    elif "Blackfeather" in hero:
+        return 12
+    elif "Phinn" in hero:
+        return 13
+    elif "Skye" in hero:
+        return 14
+    elif "Hero016" in hero: # Rona
+        return 15
+    elif "Fortress" in hero:
+        return 16
+    elif "Joule" in hero:
+        return 17
+    elif "Ardan" in hero:
+        return 18
+    elif "Hero010" in hero: # Skaarf
+        return 19
+    elif "Sayoc" in hero: # Taka
+        return 20
+    elif "Hero009" in hero: # Krul
+        return 21
+    elif "SAW" in hero:
+        return 22
+    elif "Petal" in hero:
+        return 23
+    elif "Glaive" in hero:
+        return 24
+    elif "Adagio" in hero:
+        return 26
+    elif "Ringo" in hero:
+        return 27
+    elif "Catherine" in hero:
+        return 28
+    elif "Celeste" in hero:
+        return 29
+    elif "Vox" in hero:
+        return 30
+    else:
+        return -1	
