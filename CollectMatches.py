473837be6630aa4d12ad7b20b370fdf7d34fe399
Stefan Liu <stefanliu@outlook.com>
2017-03-27 22:25:29 -0500

    Update CollectMatches.py

--- CollectMatches.py
+++ CollectMatches.py
@@ -44,8 +44,7 @@ class Collector(object):
         headers = {"Authorization": "Bearer " + cls.Key,
                            "X-TITLE-ID": "semc-vainglory",
                            "Accept": "application/vnd.api+json"}
-        res = Util._requestX(cls.URL, params=payload, headers=headers, retry=30)
-        mc = json.loads(res)
+        mc = Util._requestX(cls.URL, params=payload, headers=headers, retry=30)
         return mc		
 
     @classmethod
